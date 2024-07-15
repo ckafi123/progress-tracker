900d0c6129383551033bb63ec5e30e17c0a572ec
ckafi123 <maxamedckaafixasan@gmail.com>
2024-07-15 15:52:28 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// App.js 
+import DSAList from "./components/DSAList"; 
+
+export default function App() { 
+	return ( 
+		<> 
+			<div className="flex flex-col justify-center items-center mt-4"> 
+				<h1 className="text-emerald-500 font-bold text-3xl"> 
+					 Progress Tracker
+				</h1> 
+				<h3 className="bg-clip-text text-transparent bg-gradient-to-r 
+							from-purple-500 to-pink-500 font-bold text-xl mb-4"> 
+					 Tracker Your self
+				</h3> 
+				<DSAList /> 
+			</div> 
+		</> 
+	); 
+}
